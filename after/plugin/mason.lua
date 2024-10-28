31f8716c623e0b89dcd3d7951fbdb5de8e810969
Luke Barrier <lukebbarrier06@outlook.com>
2024-10-28 04:14:28 -0400

    Update

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -2,7 +2,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here 
   -- with the ones you want to install
-  ensure_installed = {"lua_ls", "ts_ls", "eslint", "pyright", "gopls", "html"},
+  ensure_installed = {"lua_ls", "ts_ls", "eslint", "pyright", "gopls", "html", "emmet_ls"},
   handlers = {
       function(server_name)
           if server_name == 'lua_ls' then
