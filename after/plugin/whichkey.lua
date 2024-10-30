daa42e9a7b972f999b3127de386d5ace15974270
Luke Barrier <lukebbarrier06@outlook.com>
2024-10-30 19:13:37 -0400

    Update

--- after/plugin/whichkey.lua
+++ after/plugin/whichkey.lua
@@ -5,12 +5,14 @@ end, { desc = "Buffer Local Keymaps (which-key)" })
 
 local wk = require("which-key")
 wk.add({
-  { "<leader>f", group = "Format" }, -- group
-  { "<leader>g", group = "Git" }, -- group
-  { "<leader>p", group = "Project" }, -- group
-  { "<leader>t", group = "Tree" }, -- group
-  { "<leader>c", group = "Copy" }, -- group
-  { "<leader>y", group = "Yank" }, -- group
-  { "<leader>f", group = "Format" }, -- group
-  { "<leader>s", group = "Select" }, -- group
+    { "<leader>f", group = "Format" },   -- group
+    { "<leader>g", group = "Git" },      -- group
+    { "<leader>p", group = "Project" },  -- group
+    { "<leader>t", group = "Tree" },     -- group
+    { "<leader>c", group = "Copy" },     -- group
+    { "<leader>y", group = "Yank" },     -- group
+    { "<leader>f", group = "Format" },   -- group
+    { "<leader>s", group = "Select" },   -- group
+    { "<leader>b", group = "Buffer" },   -- group
+    { "<leader>j", group = "Terminal" }, -- group
 })
