1bac5113522098ac49078fd4daa3b93c4314f2ca
Luke Barrier <lukebbarrier06@outlook.com>
2024-11-20 22:19:19 -0500

    Update

--- after/plugin/notifier.lua
+++ after/plugin/notifier.lua
@@ -2,10 +2,11 @@ require 'notifier'.setup {
     ignore_messages = {}, -- Ignore message from LSP servers with this name
     components = {        -- Order of the components to draw from top to bottom (first nvim notifications, then lsp)
         "nvim",           -- Nvim notifications (vim.notify and such)
-        "lsp"             -- LSP status updates
+        "lsp",            -- LSP status updates
+        "nvimtree"
     },
     notify = {
-        clear_time = 5000,               -- Time in milliseconds before removing a vim.notify notification, 0 to make them sticky
+        clear_time = 1000,               -- Time in milliseconds before removing a vim.notify notification, 0 to make them sticky
         min_level = vim.log.levels.INFO, -- Minimum log level to print the notification
     },
     component_name_recall = false,       -- Whether to prefix the title of the notification by the component name
--- after/plugin/notify.lua
+++ after/plugin/notify.lua
@@ -1,1 +1,25 @@
-vim.notify = require("notify")
+local notify = require("notify")
+
+notify.setup({
+    background_colour = "NotifyBackground",
+    fps = 30,
+    icons = {
+        DEBUG = "",
+        ERROR = "",
+        INFO = "",
+        TRACE = "✎",
+        WARN = ""
+    },
+    level = 2,
+    minimum_width = 50,
+    render = "simple",
+    stages = "fade",
+    time_formats = {
+        notification = "%T",
+        notification_history = "%FT%T"
+    },
+    timeout = 500,
+    top_down = true
+})
+
+vim.notify = notify
