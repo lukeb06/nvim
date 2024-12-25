297776c2a64a0fe0001608fbb5225041adccc9f1
Luke Barrier <lukebbarrier06@outlook.com>
2024-12-25 18:16:16 -0500

    Update

--- lua/core/git.lua
+++ lua/core/git.lua
@@ -1,7 +1,11 @@
+local function vimux_run_command(cmd)
+    vim.cmd(":VimuxRunCommand \"" .. cmd .. "\"")
+end
+
 -- Register new vim command: gadd
 vim.api.nvim_create_user_command("Gstatus", function(opts)
     -- Run 'git add .' in the current directory bash
-    vim.cmd(":VimuxRunCommand \"git status\"")
+    vimux_run_command("git status")
 end, {
     nargs = 0,
     desc = "Add all files to git",
@@ -9,14 +13,14 @@ end, {
 -- Register new vim command: gpush
 vim.api.nvim_create_user_command("Gpush", function(opts)
     -- Run 'git push' in the current directory bash
-    vim.cmd(":VimuxRunCommand \"git add . && git status && git commit -m 'Update' && git push\"")
+    vimux_run_command("gpush")
 end, {
     nargs = 0,
     desc = "Push all files to git",
 })
 
 vim.api.nvim_create_user_command("Prc", function(opts)
-    vim.cmd(":VimuxRunCommand \"prc\"")
+    vimux_run_command("prc")
 end, {
     nargs = 0,
     desc = "Create pull request"
