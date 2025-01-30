8dcdc43263bac723d4a35e26845fbcc3b9066e0e
Luke Barrier <lukebbarrier06@outlook.com>
2025-01-30 10:25:03 -0500

    Update

--- lua/plugins/neocord.lua
+++ lua/plugins/neocord.lua
@@ -5,7 +5,7 @@ return {
         logo_tooltip        = nil,        -- nil or string
         main_image          = "language", -- "language" or "logo"
         log_level           = nil,        -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
-        debounce_timeout    = 10,         -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
+        debounce_timeout    = 5,          -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
         blacklist           = {},         -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
         file_assets         = {},         -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
         show_time           = true,       -- Show the timer
