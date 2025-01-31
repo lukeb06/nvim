9b6d3d4f8e7b4b70de6672231e78791c79e375d1
Luke Barrier <lukebbarrier06@outlook.com>
2025-01-31 12:09:27 -0500

    Update

--- lua/core/commands/git.lua
+++ lua/core/commands/git.lua
@@ -1,7 +1,5 @@
 require("utils.vimux")
 
-
--- Git Commands
 vim.api.nvim_create_user_command("Gstatus", function()
     vimux_run_command("git status")
 end, {
@@ -25,30 +23,4 @@ vim.api.nvim_create_user_command("Gweb", function()
 end, {
     nargs = 0,
     desc = "Open git web"
-})
-
-
-
--- I like the twilight.nvim plugin, but the functionality of dim (from snacks.nvim) is much much better
-local dimmed = false
-vim.api.nvim_create_user_command("Twilight", function()
-    if dimmed then
-        require('snacks').dim.disable()
-    else
-        require('snacks').dim.enable()
-    end
-    dimmed = not dimmed
-end, {
-    nargs = 0,
-    desc = "Toggle Twilight",
-})
-
-
-
--- :Q to quit because I accidentally do capital sometimes
-vim.api.nvim_create_user_command("Q", function()
-    vim.cmd(":q")
-end, {
-    nargs = 0,
-    desc = "Quit"
 })
---
+++ lua/core/commands/init.lua
@@ -0,0 +1,24 @@
+require("core.commands.git")
+
+-- I like the twilight.nvim plugin, but the functionality of dim (from snacks.nvim) is much much better
+local dimmed = false
+vim.api.nvim_create_user_command("Twilight", function()
+    if dimmed then
+        require('snacks').dim.disable()
+    else
+        require('snacks').dim.enable()
+    end
+    dimmed = not dimmed
+end, {
+    nargs = 0,
+    desc = "Toggle Twilight",
+})
+
+
+-- :Q to quit because I accidentally do capital sometimes
+vim.api.nvim_create_user_command("Q", function()
+    vim.cmd(":q")
+end, {
+    nargs = 0,
+    desc = "Quit"
+})
