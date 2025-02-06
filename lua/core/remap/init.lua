4a2b27b4255e860f8f11c3ea67f3a6df1e692142
Luke Barrier <lukebbarrier06@outlook.com>
2025-02-06 16:30:57 -0500

    Update

--- lua/core/remap/init.lua
+++ lua/core/remap/init.lua
@@ -6,29 +6,26 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Project View", silent = 
 
 -- Tree Toggle
 vim.keymap.set("n", "<leader>tt", vim.cmd.NvimTreeFindFileToggle, { desc = "Tree Toggle", silent = true })
-vim.keymap.set("n", "<leader>pt", vim.cmd.NvimTreeFindFileToggle, { desc = "Tree Toggle", silent = true })
-
+-- vim.keymap.set("n", "<leader>pt", vim.cmd.NvimTreeFindFileToggle, { desc = "Tree Toggle", silent = true })
+--
+-- vim.keymap.set("n", "<leader>tt", vim.cmd.Neotree, { desc = "Tree Toggle", silent = true })
+vim.keymap.set("n", "<leader>pt", vim.cmd.Neotree, { desc = "Tree Toggle", silent = true })
 
 -- ESC Fix
 vim.keymap.set("n", "<ESC>", "<NOP>", { noremap = true })
 
-
 -- EZ Diagnostics
 vim.keymap.set("n", "<leader>d", ":lua vim.diagnostic.open_float()<CR>", { desc = "Diagnostics Float", silent = true })
 
-
 -- Save and Quit all buffers
-vim.keymap.set("n", "<leader>q", ":wqa<CR>", { desc = "Save and Quit", silent = true });
-
+vim.keymap.set("n", "<leader>q", ":wqa<CR>", { desc = "Save and Quit", silent = true })
 
 -- Run Selected SQL Statement
 vim.keymap.set("v", "r", '"vy:lua require("dbee").execute(vim.fn.getreg("v"))<CR>', { desc = "Run SQL", silent = true })
 
-
 -- Horizontal Scrolling ( kitty / ghostty does not have scroll direction locking. )
 vim.keymap.set("n", "L", "15zl", { desc = "Scroll right", silent = true })
 vim.keymap.set("n", "H", "15zh", { desc = "Scroll left", silent = true })
-
 
 -- Toggle Wrap
 vim.keymap.set("n", "<leader>w", ":set wrap!<CR>", { desc = "Toggle Wrap", silent = true })
