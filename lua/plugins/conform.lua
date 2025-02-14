04075008a0ac9a1abc391c6e47b6d70ac6dffac7
Luke Barrier <lukebbarrier06@outlook.com>
2025-02-14 11:24:32 -0500

    Update

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,38 +1,38 @@
 return {
-    'stevearc/conform.nvim',
-    config = function()
-        local conform = require("conform")
+	"stevearc/conform.nvim",
+	config = function()
+		local conform = require("conform")
 
-        local opts = {
-            formatters_by_ft = {
-                lua = { "stylua" },
-                markdown = { "prettier" },
-                html = { "prettier" },
-                css = { "prettier" },
-                javascript = { "prettier" },
-                typescript = { "prettier" },
-                json = { "prettier" },
-                yaml = { "prettier" },
-                typescriptreact = { "prettier" },
-                javascriptreact = { "prettier" },
-                liquid = { "prettier" },
-                python = { "ruff" },
-            },
-            format_on_save = {
-                timeout_ms = 1000,
-                lsp_format = "fallback",
-            },
-            formatters = {
-                prettier = {
-                    ft_parsers = {
-                        liquid = "babel",
-                    }
-                }
-            }
-        }
+		local opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				markdown = { "prettier" },
+				html = { "prettier" },
+				css = { "prettier" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				typescriptreact = { "prettier" },
+				javascriptreact = { "prettier" },
+				liquid = { "prettier" },
+				python = { "ruff" },
+			},
+			format_on_save = {
+				timeout_ms = 1000,
+				lsp_format = "fallback",
+			},
+			-- formatters = {
+			--     prettier = {
+			--         ft_parsers = {
+			--             liquid = "babel",
+			--         }
+			--     }
+			-- }
+		}
 
-        conform.setup(opts)
+		conform.setup(opts)
 
-        vim.keymap.set("n", "<leader>bfc", conform.format, { desc = "Format with Conform" })
-    end,
+		vim.keymap.set("n", "<leader>bfc", conform.format, { desc = "Format with Conform" })
+	end,
 }
