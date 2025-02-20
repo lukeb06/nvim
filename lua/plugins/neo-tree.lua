1eea29e1193009d73eb61a08157aa4981b099a69
Luke Barrier <lukebbarrier06@outlook.com>
2025-02-20 12:27:18 -0500

    Update

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -221,20 +221,20 @@ return {
 					hide_gitignored = true,
 					hide_hidden = true, -- only works on Windows for hidden files/directories
 					hide_by_name = {
-						--"node_modules"
+						"node_modules",
 					},
 					hide_by_pattern = { -- uses glob style patterns
 						--"*.meta",
 						--"*/src/*/tsconfig.json",
 					},
 					always_show = { -- remains visible even if other settings would normally hide it
-						--".gitignored",
+						".gitignore",
 					},
 					always_show_by_pattern = { -- uses glob style patterns
 						--".env*",
 					},
 					never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
-						--".DS_Store",
+						".DS_Store",
 						--"thumbs.db"
 					},
 					never_show_by_pattern = { -- uses glob style patterns
